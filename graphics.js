b783c6ef1b749b71b5257590d93b61ed20aae4f8
photon1503 <g.hitz@gmx.at>
2023-04-21 21:30:07 +0200

    .

--- graphics.js
+++ graphics.js
@@ -12,6 +12,7 @@ var layer = new Konva.Layer();
 stage.add(layer);
 var center = stage.width() / 2;
 
+/*
 var head = new Konva.Circle({
   x: center,
   y: 100,
@@ -29,13 +30,29 @@ var monitorMiddle = new Konva.Rect({
   stroke: "black",
   strokeWidth: 4,
 });
-// add the shape to the layer
 layer.add(monitorMiddle);
 
+*/
+
+var frontView = new Konva.Rect({
+    x: 0,
+    y: 0,
+    width: 30,
+    height: 15,
+    stroke: "black",
+    strokeWidth: 1,
+});
+layer.add(frontView);
+
 function updateGraphics() {
-  //monitorMiddle.setAttr("width", result.width);
+  /*
   monitorMiddle.setWidth(result.width);
   head.setX(monitorMiddle.x() + monitorMiddle.width() / 2);
   head.setY( screen.distance); 
+  */
+
+  frontView.setWidth(result.width);
+  frontView.setHeight(result.height);
+  frontView.setStrokeWidth(screen.bezel/10);
   layer.draw();
 }
